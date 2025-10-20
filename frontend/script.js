26cf1ede0d416c915d199dcc4adf982ee40e817f
Camilo Girardi <girardicamilo@gmail.com>
2025-10-19 21:42:50 -0300

    Backend para la aplicación conectado con el front. Configuración de Docker.

--- frontend/script.js
+++ frontend/script.js
@@ -15,17 +15,48 @@ const audioPlayer = document.getElementById('audioPlayer');
 
 // Iniciar cámara
 async function initCamera() {
-  
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+        video.srcObject = stream;
+    }  catch (err) {
+        console.error('Error while accessing the camera', err);
+        captionText.textContent = "Cannot access the camera";
+    }
 }
 
 // Capturar imagen actual del video
 function captureImage() {
-  
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0);
+    canvas.toBlob(blob => sendImageToBackend(blob), 'image/jpeg', 0.9)
 }
 
 // Enviar imagen al backend
 async function sendImageToBackend(imageBlob) {
-  
+    try {
+        const formData = new FormData();
+        formData.append('video', imageBlob, 'snapshot.jpg');
+
+        const response = await fetch('http://localhost:3000/api/detect', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (!response.ok) {
+            captionText.textContent = "Image processing failed";
+            return;
+        }
+
+        const data = await response.json();
+        console.log('Backend response:', data);
+
+        captionText.textContent = JSON.stringify(data.objects, null, 2);
+    }   catch (err) {
+        console.error('Error while sending image:', err);
+        captionText.textContent = 'Error while sending the image to the backend';
+    }
 }
 
 // Eventos
